b9026177adf903be289952388599624f77587a62
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-06-24 19:18:16 -0300

    First commit

---
+++ src/main/java/com/gabrielmsenna/Web_Service_Mongo/WebServiceMongoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielmsenna.Web_Service_Mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielmsenna/Web_Service_Mongo/WebServiceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmsenna.Web_Service_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
