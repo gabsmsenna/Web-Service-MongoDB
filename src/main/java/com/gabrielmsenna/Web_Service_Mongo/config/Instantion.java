5d5e3b2d5eac901fdcd543dc97142197f326a674
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-07-12 07:25:56 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/gabrielmsenna/Web_Service_Mongo/config/Instantion.java
@@ -0,0 +1,29 @@
+package com.gabrielmsenna.Web_Service_Mongo.config;
+
+import com.gabrielmsenna.Web_Service_Mongo.domain.User;
+import com.gabrielmsenna.Web_Service_Mongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class Instantion implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        List<User> users = List.of(new User(null, "maria", "maria@gmail.com"),
+        new User(null, "alex", "alex@gmail.com"),
+        new User(null, "bob", "bob@gmail.com"));
+
+        userRepository.saveAll(users);
+    }
+}
